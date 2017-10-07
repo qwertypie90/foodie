949f45d0a88ea0cee77103f3ef8578b0376d2ca5
Nadir <Jaffan_Nadir@hotmail.com>
2017-10-07 13:58:39 -0700

    Logged In name shows

--- asset/javascript/auth.js
+++ asset/javascript/auth.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-  $('[data-toggle=offcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active');
-   
-  });
 
   // Initialize Firebase
   var config = {
@@ -12,62 +8,63 @@ $(document).ready(function () {
     projectId: "nadir-37f82",
     storageBucket: "nadir-37f82.appspot.com",
     messagingSenderId: "78288137533"
-  				};
+  };
   firebase.initializeApp(config);
 
-//Google Auth Provider Using a popup.
+  //Google Auth Provider Using a popup.
 
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('profile');
+  provider.addScope('email');
 
 
-firebase.auth().onAuthStateChanged(function(user){
-window.user = user
-checkAuth()
+  firebase.auth().onAuthStateChanged(function (user) {
+    window.user = user
+    $('#current-user').html(window.user.displayName)
+    checkAuth()
+  });
+
+
+  $('#sign-in').click(function () {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // This gives you a Google Access Token.
+
+      window.user = result
+      console.log(result)
+
+
+      checkAuth()
+
+    }).catch(function (error) {
+      console.log(error)
+    })
+  })
 });
 
+$('#sign-out').click(function () {
 
+  firebase.auth().signOut()
+  checkAuth()
 
-$('#sign-in').click(function (){
-  	firebase.auth().signInWithPopup(provider).then(function(result) {
-		 // This gives you a Google Access Token.
-     console.log(result)
-     console.log (result.additionalUserInfo.profile.name)
-		 window.user = result.user;
-         
-         checkAuth ()
-
-								}).catch(function(error){
-                  console.log (error)
-                })
-})
-});
-
-$('#sign-out').click(function(){
-
-firebase.auth().signOut ()
 
 })
 
 
+function checkAuth() {
+  console.log(window.user)
+  if (window.user) {
 
 
-function checkAuth (){
-console.log(window.user)
-  if (window.user){
-
- 
-
-    $('#sign-in').hide ()
-    $('#sign-out').show ()
+    $('#sign-in').hide()
+    $('#sign-out').show()
 
   } else {
-    $('#sign-in').show ()
-    $('#sign-out').hide ()
+    $('#sign-in').show()
+    $('#sign-out').hide()
+
 
   }
 
 }
 
-checkAuth ()
+checkAuth()
