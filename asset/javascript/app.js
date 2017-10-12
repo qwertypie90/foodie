a502f8c396286198c963bd8e41c02d8ac1966a17
Suha Park <suhapark@Suhas-MacBook-Pro.local>
2017-10-11 20:37:06 -0700

    add showdown

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -14,183 +14,195 @@
  var view = 'articleList'
 
  $(document).ready(function() {
- 
 
- function addArticle(article) {
-     database.ref('articles').push(article);
- }
 
- function displayArticleList() {
-     database.ref('articles').once("value", function(articlesSnapshot) {
-         var listGroup = $('<div class="list-group">')
-         articlesSnapshot.forEach(function(childSnapshot) {
-             // console.log(childSnapshot.val())
-             // console.log(childSnapshot.key)
+     function addArticle(article) {
+         database.ref('articles').push(article);
+     }
 
-             //Show only the first sentence of "Content"
-             var content = childSnapshot.val().content;
-             splitContent = content.split(".")[0];
-             // console.log(splitContent);
+     function displayArticleList() {
+         database.ref('articles').once("value", function(articlesSnapshot) {
+             var listGroup = $('<div class="list-group">')
+             articlesSnapshot.forEach(function(childSnapshot) {
+                 // console.log(childSnapshot.val())
+                 // console.log(childSnapshot.key)
 
-             var listGroupItem = $('<button class="list-group-item">')
-             listGroupItem.attr('data-articleId', childSnapshot.key)
+                 //Show only the first sentence of "Content"
+                 var content = childSnapshot.val().content;
+                 splitContent = content.split(".")[0];
+                 // console.log(splitContent);
 
-             var previewTitle = $('<h3>')
-             previewTitle.html(childSnapshot.val().title)
+                 var listGroupItem = $('<button class="list-group-item">')
+                 listGroupItem.attr('data-articleId', childSnapshot.key)
 
-             var previewAuthor = $('<p>')
-             previewAuthor.html(childSnapshot.val().author)
+                 var previewTitle = $('<h3>')
+                 previewTitle.html(childSnapshot.val().title)
 
-             var previewDate = $('<p>')
-             previewDate.html(`<small>${childSnapshot.val().date}</small>`)
+                 var previewAuthor = $('<p>')
+                 previewAuthor.html(childSnapshot.val().author)
 
-             var previewText = $('<p>')
-             previewText.html(`${splitContent}...`)
+                 var previewDate = $('<p>')
+                 previewDate.html(`<small>${childSnapshot.val().date}</small>`)
 
-             listGroupItem.append(previewTitle).append(previewAuthor).append(previewDate).append(previewText)
-             listGroup.prepend(listGroupItem)
+                 var previewText = $('<p>')
+                 previewText.html(`${splitContent}...`)
+
+                 listGroupItem.append(previewTitle).append(previewAuthor).append(previewDate).append(previewText)
+                 listGroup.prepend(listGroupItem)
+             })
+
+             $('#content').html(listGroup)
          })
+     }
 
-         $('#content').html(listGroup)
-     })
- }
+     displayArticleList()
 
- displayArticleList()
+     // youtube API function
+     function search(query, cb) {
+         $.ajax({
+             cache: false,
+             data: $.extend({
+                 key: 'AIzaSyCrAhV6_zQqKuURLfhF_xcXAhY73ydW2Ug',
+                 q: query,
+                 part: 'snippet',
+                 type: 'video'
+             }, { maxResults: 6 }),
+             dataType: 'json',
+             type: 'GET',
+             timeout: 5000,
+             url: 'https://www.googleapis.com/youtube/v3/search'
+         }).done(function(data) {
 
- // youtube API function
- function search(query, cb) {
-     $.ajax({
-         cache: false,
-         data: $.extend({
-             key: 'AIzaSyCrAhV6_zQqKuURLfhF_xcXAhY73ydW2Ug',
-             q: query,
-             part: 'snippet',
-             type: 'video'
-         }, { maxResults: 6 }),
-         dataType: 'json',
-         type: 'GET',
-         timeout: 5000,
-         url: 'https://www.googleapis.com/youtube/v3/search'
-     }).done(function(data) {
-
-         cb(data.items)
-     })
- }
-
-
- function displayArticle(articleId) {
-     database.ref(`articles/${articleId}`).once('value', function(snapshot) {
-         var article = $('<div>')
-
-         var buttonBack = $('<button class="btn btn-outline-info">')
-         buttonBack.html('Back')
-         buttonBack.click(function() {
-             displayArticleList()
+             cb(data.items)
          })
+     }
 
-         var articleTitle = $('<h3>')
-         articleTitle.html(snapshot.val().title)
 
-         var articleAuthor = $('<p>')
-         articleAuthor.html(snapshot.val().author)
+     function displayArticle(articleId) {
+         database.ref(`articles/${articleId}`).once('value', function(snapshot) {
+             var article = $('<div>')
 
-         var articleDate = $('<p>')
-         articleDate.html(`<small>${snapshot.val().date}</small>`)
+             // var buttonBack = $('<button class="btn btn-outline-info">')
+             // buttonBack.html('Back')
+             // buttonBack.click(function() {
+             //     displayArticleList()
+             // })
 
-         var articleText = $('<p>')
-         articleText.html(snapshot.val().content)
+             var articleTitle = $('<h3>')
+             articleTitle.html(snapshot.val().title)
 
-         var gallery = $('<div class="vids">')
+             var articleAuthor = $('<p>')
+             articleAuthor.html(snapshot.val().author)
 
-         article.append(buttonBack)
-         // article.append(`<pre>${JSON.stringify(snapshot.val(), null, 2)}</pre>`)
-         article.append(articleTitle).append(articleAuthor).append(articleDate).append(articleText)
+             var articleDate = $('<p>')
+             articleDate.html(`<small>${snapshot.val().date}</small>`)
 
-         $('#content').html(article)
-         $('form').hide()
+             var converter = new showdown.Converter()
 
-         article.append(gallery)
+             var html = converter.makeHtml(snapshot.val().content);
 
-         search(snapshot.val().title, function(data) {
-             // console.log(data)
+             var articleText = $('<p>')
+             articleText.html(html)
 
-             //loop through the videos under the article
-             for (var i = 0; i < data.length; i++) {
-                 // console.log(data[i].snippet.title)
-                 var vidDiv = $("<div>");
-                 var vidTitle = $('<p class="title">').text(data[i].snippet.title);
-                 // Creating and storing an image tag
-                 var vidImage = $('<img style="cursor:pointer">');
-                 vidImage.addClass("video-pic");
-                 vidImage.attr("src", data[i].snippet.thumbnails.default.url);
-                 // console.log(results.items[i].snippet.title);
-                 var results = data[i].snippet.title;
-                 var id = data[i].id.videoId;
-                 var URL = "https://www.youtube.com/watch?v=" + id
-                 var vidTitle = $('<p class="title">').text(data[i].snippet.title);
+             var gallery = $('<div class="vids">')
 
-                 vidDiv.append(vidImage);
-                 vidDiv.append(vidTitle);
-                 $(".vids").prepend(vidDiv);
+             // article.append(buttonBack)
+             // article.append(`<pre>${JSON.stringify(snapshot.val(), null, 2)}</pre>`)
+             article.append(articleTitle).append(articleAuthor).append(articleDate).append(articleText)
 
-                 var videoPic = $(".video-pic")
-                 videoPic.on("click", function() {
-                     event.preventDefault();
-                     window.open(URL);
-                 })
-             }
-             // console.log(data[0].snippet.title)
+             $('#content').html(article)
+             $('form').hide()
+
+             article.append(gallery)
+
+             search(snapshot.val().title, function(data) {
+                 // console.log(data)
+
+                 //loop through the videos under the article
+                 for (var i = 0; i < data.length; i++) {
+                     // console.log(data[i].snippet.title)
+                     var vidDiv = $("<div>");
+                     var vidTitle = $('<p class="title">').text(data[i].snippet.title);
+                     // Creating and storing an image tag
+                     var vidImage = $('<img style="cursor:pointer">');
+                     vidImage.addClass("video-pic");
+                     vidImage.attr("src", data[i].snippet.thumbnails.default.url);
+                     // console.log(results.items[i].snippet.title);
+                     var results = data[i].snippet.title;
+                     var id = data[i].id.videoId;
+                     var URL = "https://www.youtube.com/watch?v=" + id
+                     var vidTitle = $('<p class="title">').text(data[i].snippet.title);
+
+                     vidDiv.append(vidImage);
+                     vidDiv.append(vidTitle);
+                     $(".vids").prepend(vidDiv);
+
+                     var videoPic = $(".video-pic")
+                     videoPic.on("click", function() {
+                         event.preventDefault();
+                         window.open(URL);
+                     })
+                 }
+                 // console.log(data[0].snippet.title)
+
+             })
+
 
          })
+     }
 
+     $('#content').on("click", ".list-group-item", function() {
+
+         var articleId = $(this).attr('data-articleId')
+         view = "fullArticle"
+         displayArticle(articleId);
+         var title = $(this).attr('data-articleId')
+
+     });
+
+     // ADDING ARTICLE TO FIREBASE
+     $(".submit").on("click", function(event) {
+         console.log("button pressed");
+         event.preventDefault();
+         var nTitle = $("#example-text-input").val().trim();
+         // console.log(nTitle);
+
+         // Adding celeb from the textbox to our array
+         var nAuthor = $("#example-search-input").val().trim();
+         // console.log(nAuthor);
+         // Adding celeb from the textbox to our array
+         var nMoment = moment().format('MMMM Do YYYY, h:mm:ss a')
+         // console.log(nMoment);
+         // Adding celeb from the textbox to our array
+         var nContent = $("#exampleTextarea").val().trim();
+         // console.log(nContent);
+
+         var article = {
+             title: nTitle,
+             author: nAuthor,
+             date: nMoment,
+             content: nContent
+         };
+
+         // console.log(article);
+
+         $("#example-text-input").val("");
+         $("#exampleTextarea").val("");
+
+         function addArticle() {
+             database.ref('/articles').push(article);
+             displayArticleList();
+         }
+
+         addArticle();
+     })
+
+
+
+     $("#sidebarCollapse").on("click", function(event) {
+         document.getElementById('sidebar').style.width = "200px";
+         // $('#main').css('background', 'black')
 
      })
- }
-
- $('#content').on("click", ".list-group-item", function() {
-
-     var articleId = $(this).attr('data-articleId')
-     view = "fullArticle"
-     displayArticle(articleId);
-     var title = $(this).attr('data-articleId')
-
- });
-
- // ADDING ARTICLE TO FIREBASE
- $(".submit").on("click", function(event) {
- console.log("button pressed");
- event.preventDefault();
- var nTitle = $("#example-text-input").val().trim();
- // console.log(nTitle);
-
- // Adding celeb from the textbox to our array
- var nAuthor = $("#example-search-input").val().trim();
- // console.log(nAuthor);
- // Adding celeb from the textbox to our array
- var nMoment = moment().format('MMMM Do YYYY, h:mm:ss a')
- // console.log(nMoment);
- // Adding celeb from the textbox to our array
- var nContent = $("#exampleTextarea").val().trim();
- // console.log(nContent);
-
- var article = {
-     title: nTitle,
-     author: nAuthor,
-     date: nMoment,
-     content: nContent
- };
-
- // console.log(article);
-
- $("#example-text-input").val("");
- $("#exampleTextarea").val("");
-
- function addArticle() {
-     database.ref('/articles').push(article);
-     displayArticleList();
- }
-
- addArticle();
- })
 
  });
